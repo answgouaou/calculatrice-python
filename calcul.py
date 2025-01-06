c3ac593eefcb57a37c85f048048e4a94634bddca
Walid GOUAOU <walid.gouaou@ansys.com>
2025-01-06 12:41:03 +0100

    ajout de la fonction soustraction

--- calcul.py
+++ calcul.py
@@ -1,7 +1,10 @@
 def addition(a, b):
     return a + b
+def soustraction(a, b):
+    return a - b
 
 if __name__ == "__main__":
     x = 5
     y = 10
     print(f"La somme de {x} et {y} est {addition(x, y)}")
+    print(f"La soustraction de {x} et {y} est {soustraction(x, y)}")
